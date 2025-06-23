c53ccce9d698f7f196f147818bd7d35c2ca3208b
P4m3l4-dev <pamela.a11@aluno.ifsc.edu.br>
2025-06-23 11:34:29 -0300

    desenvolvimento

---
+++ usuarios/src/main/java/cadastro/CadastroUsuarioServlet.java
@@ -0,0 +1,70 @@
+package cadastro;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+@WebServlet("/cadastroUsuario")
+public class CadastroUsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CadastroUsuarioServlet() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		// Pegando os dados do formulário HTML pelo request
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String telefone = request.getParameter("telefone");
+		String senha = request.getParameter("senha");
+		// Gerando o JSON para enviar ao webservice
+		JSONObject json = new JSONObject();
+		json.put("nome", nome);
+		json.put("email", email);
+		json.put("telefone", telefone);
+		json.put("senha", senha);
+		// Definindo o endpoint (URL) do web service
+		URL url = new URL("http://localhost/cadusuario.php");
+		// Criando o objeto para conexão HTTP
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		// Configurando a conexão
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json; utf-8");
+		conn.setRequestProperty("Accept", "application/json");
+		conn.setDoOutput(true);
+		try {
+			// Enviando o json gerado pelo request
+			OutputStream os = conn.getOutputStream();
+			byte[] input = json.toString().getBytes("utf-8");
+			os.write(input, 0, input.length);
+			// recebendo a resposta (response) do web service
+			StringBuilder responseContent = new StringBuilder();
+			InputStreamReader isr = new InputStreamReader(conn.getInputStream(), "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				responseContent.append(line.trim());
+			}
+			// Enviando response para o cliente http
+			response.setContentType("application/json");
+			response.getWriter().write(responseContent.toString());
+		} catch (Error e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
